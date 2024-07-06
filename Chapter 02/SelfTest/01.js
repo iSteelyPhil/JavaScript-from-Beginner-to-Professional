d5a1d185aca2a6d7aef994f88d8b193089594846
Phillip <philmography2010@gmail.com>
2024-07-06 09:01:16 -0500

    update

--- Chapter 02/SelfTest/01.js
+++ Chapter 02/SelfTest/01.js
@@ -1,13 +1,3 @@
-let str1 = 'Laurence'; 
-let str2 = "Svekis"; 
-let val1 = undefined;
-let val2 = null;
-let myNum = 1000;
-
-
-console.log(typeof(str1));
-console.log(typeof(str2));
-console.log(typeof(val1));
-console.log(typeof(val2));
-console.log(typeof(myNum));
-
+let x = 5;
+let y = "5";
+console.log(x != y);
